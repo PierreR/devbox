92ae588d58ea1060245b480efdc896e69ecceee4
Pierre Radermecker <pradermecker@cirb.brussels>
2017-03-18 00:10:12 +0100

    Don't pinned nixpkgs

--- release.nix
+++ release.nix
@@ -1,33 +1,21 @@
 let
-  _pkgs = import <nixpkgs> { };
-
-  _nixpkgs = builtins.fromJSON (builtins.readFile ./.nixpkgs.json);
-
-  _src = _pkgs.fetchFromGitHub {
-    owner = "NixOS";
-    repo  = "nixpkgs";
-    inherit (_nixpkgs) rev sha256;
-  };
-
-  pkgs = import _src { };
-  hghc = pkgs.haskellPackages;
-  hlib = pkgs.haskell.lib;
-  henv = hghc.ghcWithPackages (p: with p; [dhall_git protolude_git turtle ]);
-
+  pkgs = import <nixpkgs> { };
+  henv = pkgs.haskellPackages.ghcWithPackages (p: with p; [dhall_git protolude_git turtle ]);
 in
 {
-  # We create an 'shell' suitable for interpreting the script
+  # We create a 'shell' suitable for interpreting the script
   user = pkgs.stdenv.mkDerivation {
     name = "user";
     buildInputs = [
       henv
-      _pkgs.ruby
-      _pkgs.asciidoctor
-      _pkgs.mr
-      _pkgs.rsync
-      _pkgs.curl
+      pkgs.ruby
+      pkgs.asciidoctor
+      pkgs.mr
+      pkgs.rsync
+      pkgs.curl
     ];
   };
+  # We create a 'env' suitable for installing in user space
   cicd-shell = pkgs.buildEnv {
     name = "cicd-shell";
     paths = [
