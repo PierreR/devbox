417e4b68c1739320ddf06f8d04767943fe9bedc9
Pierre Radermecker <pradermecker@cirb.brussels>
2017-04-25 22:14:48 +0200

    dhall_git -> dhall

--- release.nix
+++ release.nix
@@ -1,7 +1,7 @@
 let
   pkgs = import <nixpkgs> {};
   pinned = import ./nixpkgs/pin.nix;
-  henv = pinned.haskellPackages.ghcWithPackages (p: with p; [dhall_git protolude_git turtle ]);
+  henv = pinned.haskellPackages.ghcWithPackages (p: with p; [dhall protolude_git turtle ]);
 in
 {
   # We create a 'shell' suitable for interpreting the script
