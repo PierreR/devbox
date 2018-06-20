39e51aed2e3ba1099d76d8a09175ff87d43b0906
Pierre Radermecker <pradermecker@cirb.brussels>
2018-06-20 15:50:47 +0200

    Add desktop-kde-configuration file

---
+++ system/desktop-kde-configuration.nix
@@ -0,0 +1,18 @@
+# All the ui manager required for the ui configuration
+# such as desktopManager, windowManager and displayManager
+{pkgs, config, ... }:
+
+{
+  services.xserver = {
+    desktopManager = {
+      plasma5.enable = true;
+    };
+    displayManager = {
+      lightdm = {
+        enable = true;
+        autoLogin.user= "vagrant";
+        autoLogin.enable= true;
+      };
+    };
+  };
+}
