a07bd8f6622dccc6416992d64b9b0d52f42a50b8
Pierre Radermecker <pradermecker@cirb.brussels>
2018-11-04 20:35:07 +0100

    Update cicd-shell build version

--- user/setenv.hs
+++ user/setenv.hs
@@ -30,7 +30,7 @@ eclipseVersion = "4.7"
 eclipseFullVersion = eclipseVersion <> ".3"
 eclipsePluginList = ["jdt", "yedit", "testng"]
 
-cicdshellTag =  "v2.5.7"
+cicdshellTag =  "v2.5.10"
 
 mrRepoUrl = "git://github.com/CIRB/vcsh_mr_template.git"
 
