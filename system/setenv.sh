PierreR/devbox

commit bdc31bbb9957906b170613cd1879e8adb501fca0

Add nixbox.sh script

system/setenv.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/env nix-shell
 #! nix-shell -i bash -p rsync vcsh dhall-bash
 
-scriptDir="$(dirname -- "$(readlink -f -- "$0")")"
 
-source "$scriptDir/../utils.sh"
+script_dir="$(dirname -- "$(readlink -f -- "$0")")"
+
+source "$script_dir/utils.sh"
 
 config_file=$1
 if [ ! -f "$config_file" ]; then
@@ -34,7 +35,7 @@ sync_extra_config () {
         if ! $sync
         then
             echo "No personal configuration found. Overridding ${config} using the devbox source repository"
-            cp --verbose "${scriptDir}/${config}" "/etc/nixos/${config}"
+            cp --verbose "${script_dir}/${config}" "/etc/nixos/${config}"
         fi
     fi
 }
@@ -42,8 +43,8 @@ sync_extra_config () {
 # Always override the packer custom-configuration file
 if ! $sync
 then
-  cp --verbose "${scriptDir}/custom-configuration.nix" "/etc/nixos/custom-configuration.nix";
-  cp --verbose "${scriptDir}/puppetdb-dns.nix" "/etc/nixos/puppetdb-dns.nix";
+  cp --verbose "${script_dir}/custom-configuration.nix" "/etc/nixos/custom-configuration.nix";
+  cp --verbose "${script_dir}/puppetdb-dns.nix" "/etc/nixos/puppetdb-dns.nix";
 else
   echo "About to sync files."
 fi
@@ -59,7 +60,7 @@ rm -f /etc/nixos/desktop-configuration.nix
 ln -s /etc/nixos/desktop-tiling-configuration.nix /etc/nixos/desktop-configuration.nix
 
 # Sync system custom nixpkgs files
-rsync -qav --chmod=644 "${scriptDir}/pkgs/" /etc/cicd/
+rsync -qav --chmod=644 "${script_dir}/pkgs/" /etc/cicd/
 
 set +e
 echo "Updating ..."

nixbox.sh
@@ -1,0 +1,36 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p dhall-bash
+
+script_dir="$(dirname -- "$(readlink -f -- "$0")")"
+
+source "$script_dir/utils.sh"
+
+config_file=$1
+if [ ! -f "$config_file" ]; then
+    _failure "please pass the path to a valid dhall configuration file as first argument. Can't find ${config_file}."
+    exit 1
+fi
+
+eval $(dhall-to-bash --declare mount_dir <<< "($config_file).mountDir")
+
+dest_dir="${mount_dir}/nixbox"
+if [ ! -d "$dest_dir"  ]; then
+  echo "Cloning nixbox."
+  git clone https://github.com/nix-community/nixbox.git "$dest_dir"
+fi
+
+dest_script_dir="${dest_dir}/scripts"
+cp "${script_dir}/system/custom-configuration.nix" "$dest_script_dir"
+cp "${script_dir}/system/local-configuration.nix" "$dest_script_dir"
+cp "${script_dir}/system/desktop-tiling-configuration.nix" "$dest_script_dir"
+
+append=\
+'curl -sf "$packer_http/local-configuration.nix" > /mnt/etc/nixos/local-configuration.nix  \
+curl -sf "$packer_http/desktop-tiling-configuration.nix" > /mnt/etc/nixos/desktop-tiling-configuration.nix
+'
+
+dest_install_script="${dest_script_dir}/install.sh"
+if ! grep -q "local-configuration" "$dest_install_script";  then
+ echo "Append local and desktop to the install script."
+ sed -i "/^curl -sf.*custom-configuration.nix.*/a ${append}" "$dest_install_script"
+fi

commit 307b2415f62c07e5f3429dc1bfc8ca3b12affa7a

fix(): Regression from bdc31bb

system/setenv.sh
@@ -4,7 +4,7 @@
 
 script_dir="$(dirname -- "$(readlink -f -- "$0")")"
 
-source "$script_dir/utils.sh"
+source "$script_dir/../utils.sh"
 
 config_file=$1
 if [ ! -f "$config_file" ]; then