842b44b25e8fcd75c2debda3dea3e80a28abc62b
Pierre Radermecker <pradermecker@cirb.brussels>
2019-09-28 18:00:18 +0200

    Remove lorri from system configuration

--- system/setenv.sh
+++ system/setenv.sh
@@ -32,9 +32,6 @@ sync_extra_config () {
 
 sync_extra_config "custom-configuration.nix"
 
-rsync -qav --chmod=644 "${script_dir}/nix/" /etc/nixos/
-rsync -ai --chmod=644 "${script_dir}/lorri.nix" /etc/nixos/lorri.nix
-
 set +e
 echo "Updating ..."
 /usr/bin/env time -f "Completed after %E min" nixos-rebuild switch > "${mount_dir}/system_boot.log" 2>&1
