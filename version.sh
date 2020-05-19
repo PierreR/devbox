5abffc3d62a829638624dcb42c4fdd2a14dcda95
Pierre Radermecker <pradermecker@cirb.brussels>
2020-05-19 19:41:48 +0200

    Update version

--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 release="20.03"
-version="${release}.0"
-scm_uri="http://stash.cirb.lan/rest/api/latest/projects/DEVB/repos/devbox/archive?format=tgz&at=v${version}"
+version="${release}.1"
+scm_uri="http://stash.cirb.lan/rest/api/latest/projects/CICD/repos/devbox/archive?format=tgz&at=v${version}"
