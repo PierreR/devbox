942f81dd456d9e7429397d068aafbca8fbd06620
Pierre Radermecker <pradermecker@cirb.brussels>
2020-06-06 00:35:34 +0200

    Add bash prompt

--- .config/nixpkgs/modules/profiles/bash.nix
+++ .config/nixpkgs/modules/profiles/bash.nix
@@ -20,6 +20,10 @@ in
       shellAliases = alias;
       initExtra = ''
         path+="$HOME/.local/bin"
+        GIT_PS1_SHOWUNTRACKEDFILES="show"
+        GIT_PS1_SHOWDIRTYSTATE="show"
+        source ~/.config/nixpkgs/modules/assets/git-prompt.sh
+        PS1='\n\[\033[1;32m\]\w \[\033[0m\]\[\033[01;34m\]$(__git_ps1 " %s \n\n")\[\033[0m\]→ '
       '';
     };
   };
