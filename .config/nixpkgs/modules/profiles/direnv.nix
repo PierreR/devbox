c3988fba079113c11764070f31e283c142363401
Jean-François Roche <jfroche@affinitic.be>
2020-09-07 14:10:36 +0200

    feat(direnv): use nix-direnv
    Prevent garbage collection to remove derivations built by direnv.
    Speed up nix-shell evaluation using caching.
    
    We don't store .direnv cached files in each project directory but in
    ~/.cache/direnv/layouts
    
    More info here: https://github.com/nix-community/nix-direnv

---
+++ .config/nixpkgs/modules/profiles/direnv.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+let
+  cfg = config.profiles.direnv;
+
+in
+{
+  options = {
+    profiles.direnv = {
+      enable = mkEnableOption "direnv";
+    };
+  };
+  config = mkIf cfg.enable {
+    programs.direnv.enable = true;
+    programs.direnv.enableNixDirenvIntegration = true;
+
+    home.file.".config/direnv/lib/nix.sh" = {
+      text = "
+: \${XDG_CACHE_HOME:=$HOME/.cache}
+pwd_hash=$(echo -n $PWD | shasum | cut -d ' ' -f 1)
+mkdir -p $XDG_CACHE_HOME/direnv/layouts
+direnv_layout_dir=$XDG_CACHE_HOME/direnv/layouts/$pwd_hash
+";
+      executable = true;
+    };
+  };
+
+}
