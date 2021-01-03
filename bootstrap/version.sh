aed5323771cef35e843e2d345137163b5804e8da
Pierre Radermecker <pradermecker@cirb.brussels>
2021-01-03 15:38:33 +0100

    Update version & CHANGELOG for 21.03

--- bootstrap/version.sh
+++ bootstrap/version.sh
@@ -1,3 +1,3 @@
-release="20.09"
-version="${release}.1"
+release="21.05"
+version="${release}.0"
 scm_uri="http://stash.cirb.lan/rest/api/latest/projects/CICD/repos/devbox/archive?format=tgz&at=v${version}"
