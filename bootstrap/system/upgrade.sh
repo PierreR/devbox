a6ec33f5d72843ad29a2cd6c1a72cdbcba7d7c6c
Jean-François Roche <jfroche@affinitic.be>
2021-06-14 10:48:55 +0200

    Add script to upgrade to 21.05 in place
    curl -f http://stash.cirb.lan/projects/CICD/repos/devbox/raw/bootstrap/system/upgrade.sh?at=refs%2Fheads%2F21.05 | bash

---
+++ bootstrap/system/upgrade.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -xeu
+vcsh run dotfiles git fetch
+vcsh run dotfiles git co 21.05
+vcsh dotfiles pull --quiet --rebase --ff-only
+nix-channel --update
+sudo bash -c "curl http://stash.cirb.lan/projects/CICD/repos/devbox/raw/bootstrap/nixbox/scripts/configuration.nix?at=refs%2Fheads%2F21.05 > /etc/nixos/configuration.nix"
+sudo sed -i "s/20.09/21.05/" /vagrant/local-configuration.nix
+sudo /home/vagrant/bootstrap/system/setenv.sh
+reboot
