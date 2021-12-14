04002696c4f425fbc5ae59c9a36b100885b96858
Jean-François Roche <jfroche@affinitic.be>
2021-12-14 11:14:27 +0100

    fix: root channel
    Default NIX_PATH is using root channels for nixpkgs:
    NIX_PATH=/home/vagrant/.nix-defexpr/channels:nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=/etc/nixos/configuration.nix:/nix/var/nix/profiles/per-user/root/channels:nixpkgs-overlays=http://stash.cirb.lan/CICD/nixpkgs-overlays/archive/21.11.tar.gz:nixpkgs-overlays=https://bitbucket.irisnet.be/CICD/nixpkgs-overlays/archive/21.11.tar.gz
    
    We need to update the root channels.

--- bootstrap/system/upgrade.sh
+++ bootstrap/system/upgrade.sh
@@ -4,6 +4,12 @@ vcsh run dotfiles git fetch
 vcsh run dotfiles git co 21.11
 vcsh dotfiles pull --quiet --rebase --ff-only
 nix-channel --update
+cat <<EOF | sudo tee /root/.nix-channels
+https://github.com/nix-community/home-manager/archive/6ce1d64073f48b9bc9425218803b1b607454c1e7.tar.gz home-manager
+https://releases.nixos.org/nixos/21.11/nixos-21.11.334139.1bd4bbd49be/nixexprs.tar.xz nixpkgs
+https://releases.nixos.org/nixos/21.11/nixos-21.11.334139.1bd4bbd49be/nixexprs.tar.xz nixos
+EOF
+sudo nix-channel --update
 sudo bash -c "curl https://bitbucket.irisnet.be/projects/CICD/repos/devbox/raw/bootstrap/nixbox/scripts/configuration.nix?at=refs%2Fheads%2F21.11 > /etc/nixos/configuration.nix"
 sudo sed -i "s/21.05/21.11/" /vagrant/local-configuration.nix
 sudo /home/vagrant/bootstrap/system/setenv.sh
