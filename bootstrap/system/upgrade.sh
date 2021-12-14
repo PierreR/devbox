f5be120491f1aecb8c8c3976c0b882bf7f06899d
Jean-François Roche <jfroche@affinitic.be>
2021-12-14 11:23:57 +0100

    fix(upgrade): /vagrant/local-configuration.nix might not exist
    Some user don't have customization

--- bootstrap/system/upgrade.sh
+++ bootstrap/system/upgrade.sh
@@ -11,6 +11,6 @@ https://releases.nixos.org/nixos/21.11/nixos-21.11.334139.1bd4bbd49be/nixexprs.t
 EOF
 sudo nix-channel --update
 sudo bash -c "curl https://bitbucket.irisnet.be/projects/CICD/repos/devbox/raw/bootstrap/nixbox/scripts/configuration.nix?at=refs%2Fheads%2F21.11 > /etc/nixos/configuration.nix"
-sudo sed -i "s/21.05/21.11/" /vagrant/local-configuration.nix
+test -f /vagrant/local-configuration.nix && sudo sed -i "s/21.05/21.11/" /vagrant/local-configuration.nix
 sudo /home/vagrant/bootstrap/system/setenv.sh
-reboot
+sudo reboot
