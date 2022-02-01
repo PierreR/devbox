35768026c09aab2556e256c4d56b9ebb2687d715
Jean-François Roche <jfroche@affinitic.be>
2022-02-01 23:21:21 +0000

    feat(wsl): add wsl script
    Install the minimum configuration for nix to work in other Linux distribution.
    
    To run in wsl:
    
    bash <(curl -L "https://bitbucket.irisnet.be/projects/CICD/repos/devbox/raw/bootstrap/wsl.sh?at=refs%2Fheads%2F21.11")
    
    Tested with WSL ubuntu 20.04

---
+++ bootstrap/wsl.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eux
+RELEASE="21.11"
+command -v nix >/dev/null 2>&1 || sh <(curl -L https://releases.nixos.org/nix/nix-2.5.1/install) --no-daemon
+test -d devbox || git clone https://bitbucket.irisnet.be/scm/cicd/devbox.git
+git -C devbox checkout "${RELEASE}"
+git -C devbox pull
+cp devbox/.nix-channels .
+nix-channel --update
+grep -qF 'NIX_PATH' .profile || echo 'export NIX_PATH=$HOME/.nix-defexpr/channels:nixpkgs=/nix/var/nix/profiles/per-user/$USER/channels/nixos:nixpkgs-overlays=https://bitbucket.irisnet.be/CICD/nixpkgs-overlays/archive/${RELEASE}.tar.gz' >> .profile
+source .profile
+mkdir -p .config/nix
+cp devbox/.config/nix/nix.conf .config/nix
+sudo rsync -av --include '*.crt' --exclude '*' devbox/bootstrap/system/ /usr/local/share/ca-certificates/
+sudo update-ca-certificates
+command -v home-manager >/dev/null 2>&1 || nix-shell '<home-manager>' -A install -I "home-manager=https://github.com/rycee/home-manager/archive/release-${RELEASE}.tar.gz"
+rsync -av devbox/.config/nixpkgs/modules/ .config/nixpkgs/modules/
+envsubst < devbox/bootstrap/user/wsl.nix.tmpl > ~/.config/nixpkgs/home.nix
+home-manager switch
+grep -qF 'direnv' .bashrc || echo 'eval "$(/home/jfroche/.nix-profile/bin/direnv hook bash)"' >> .bashrc
+source .bashrc
